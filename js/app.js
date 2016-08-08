d27edcf7eb7073e11dab4b445db4d1a8eb6dce4d
Shaunak Sen <shaunak1105@gmail.com>
2016-08-08 10:53:27 +0530

    routes configured

--- js/app.js
+++ js/app.js
@@ -1,3 +1,54 @@
-/**
- * Created by shaunak on 3/8/16.
- */
+var myApp = angular.module('feedbackApp', ['ui.router', 'ngResource'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('app', {
+                url: '/',
+                views: {
+                    'header': {
+                        templateUrl: 'templates/header.html'
+                    },
+                    'content': {
+                        templateUrl: 'templates/content.html'
+                    }
+                }
+            })
+            .state('app.aboutus', {
+                url: 'aboutus',
+                views: {
+                    'content@': {
+                        templateUrl: 'views/aboutus.html',
+                        controller: 'AboutController'
+                    }
+                }
+            })
+            .state('app.contactus', {
+                url: 'contactus',
+                views: {
+                    'content@': {
+                        templateUrl: 'views/contactus.html',
+                        controller: 'ContactController'
+                    }
+                }
+            })
+            .state('app.menu', {
+                url: 'menu',
+                views: {
+                    'content@': {
+                        templateUrl: 'views/menu2.html',
+                        controller: 'MenuController'
+                    }
+                }
+            })
+            .state('app.dishdetails', {
+                url: 'menu/:id',
+                views: {
+                    'content@': {
+                        templateUrl: 'views/dishdetail.html',
+                        controller: 'dishDetailController'
+                    }
+                }
+            });
+        $urlRouterProvider.otherwise('/');
+    });
+
+
